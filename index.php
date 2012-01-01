1012cce6e307ba598e6a67ee4f317daca86aa60e
Patrik Lamberger <itutvecklare@gmail.com>
2012-01-01 11:49:15 +0100

    Update
    Some minor updates in index.php and single.php

--- index.php
+++ index.php
@@ -1,60 +1,63 @@
 <?php get_header(); ?>
-
-	<?php if (have_posts()) : ?>
-
-	<?php while (have_posts()) : the_post(); ?>
-	
-	<div class="container">
+<div class="container">
 	<div class="row">
+		
 		
 		<div class="eightcol">
 			
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-		
-			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time('Y-m-d') ?>" pubdate><?php the_time('F jS, Y') ?></time> <!-- by <?php the_author() ?> -->
+		<?php if (have_posts()) : ?>
+		<?php while (have_posts()) : the_post(); ?>
 			
-			<?php the_content('Read the rest of this entry &raquo;'); ?>
+		<article <?php post_class() ?>>
+		
+			<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+			<time datetime="<?php the_time('Y-m-d') ?>" pubdate><?php the_time('l, F jS, Y') ?></time>
+			
+			<?php the_excerpt() ?>
 			
 			<footer><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></footer>
 		
 		</article>
+
+		<?php endwhile; ?>
+
+		<?php if (show_posts_nav()) : ?>
+		<nav>
+			<ul>
+				<li><?php next_posts_link('&laquo; Older Entries') ?></li>
+				<li><?php previous_posts_link('Newer Entries &raquo;') ?></li>
+			</ul>
+		</nav>
+		<?php endif; ?>
+
+		<?php else :
+
+		if ( is_category() ) { // If this is a category archive
+			printf("<h2>Sorry, but there aren't any posts in the %s category yet.</h2>", single_cat_title('',false));
+		} else if ( is_date() ) { // If this is a date archive
+			echo("<h2>Sorry, but there aren't any posts with this date.</h2>");
+		} else if ( is_author() ) { // If this is a category archive
+			$userdata = get_userdatabylogin(get_query_var('author_name'));
+			printf("<h2>Sorry, but there aren't any posts by %s yet.</h2>", $userdata->display_name);
+		} else {
+			echo("<h2>No posts found.</h2>");
+		}
+		get_search_form();
+
+		endif;
+		?>
+			
+		</div><!-- End eightcol -->  
 		
-		</div><!-- End eightcol -->
-	
+		
+		
 		<div class="fourcol last">
 		
-			<?php get_sidebar() ?>
-
-			<?php endwhile; ?>
+			<?php get_sidebar(); ?>
 			
-			<?php if (show_posts_nav()) : ?>
-				<nav>
-					<ul>
-						<li><?php next_posts_link('&laquo; Older Entries') ?></li>
-						<li><?php previous_posts_link('Newer Entries &raquo;') ?></li>
-					</ul>
-				</nav>
-			<?php endif; ?>
-
-			<?php else : ?>
-			<div class="container">
-				<div class="row">
-					<div class="twelvecol">
-						<h2>Not Found</h2>
-							<p>Sorry, but you are looking for something that isn't here.</p>
-							<?php get_search_form(); ?>
-			
-					</div><!-- End twelvecol -->
-				</div><!-- End row-->
-			</div><!-- End container -->
-			<?php endif; ?>
-			
-
-		</div><!-- End fourcol-->
+		</div><!-- End fourcol -->
 
 	</div><!-- End row-->
 
 </div><!-- End container -->
-
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -7,17 +7,7 @@
 			
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<?php previous_post_link('&laquo; %link') ?> <?php next_post_link('%link &raquo;') ?>
-
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-		
-			<h2><?php the_title(); ?></h2>
-			
-			<?php the_content('<p>Read the rest of this entry &raquo;</p>'); ?>
-			<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-			<?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
-			
-			<footer>	
+			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 				This entry was posted
 				<?php /* This is commented, because it requires a little adjusting sometimes.
 				You'll need to download this plugin, and follow the instructions:
@@ -25,11 +15,19 @@
 				/* $entry_datetime = abs(strtotime($post->post_date) - (60*120)); echo time_since($entry_datetime); echo ' ago'; */ ?>
 				on <time datetime="<?php the_time('Y-m-d') ?>" pubdate><?php the_time('l, F jS, Y') ?> at <?php the_time() ?></time>
 				and is filed under <?php the_category(', ') ?>.
-				You can follow any responses to this entry through the <?php post_comments_feed_link('RSS 2.0'); ?> feed.
+			<h2><?php the_title(); ?></h2>
+			
+			<?php the_content('<p>Read the rest of this entry &raquo;</p>'); ?>
+			<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+			<?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
+			
+			<footer>	
+				
+				Follow responses <?php post_comments_feed_link('RSS 2.0'); ?> feed.
 
 				<?php if ( comments_open() && pings_open() ) {
 				// Both Comments and Pings are open ?>
-				You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.
+				Leave a<a href="#respond"> response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.
 
 				<?php } elseif ( !comments_open() && pings_open() ) {
 				// Only Pings are Open ?>
@@ -43,8 +41,8 @@
 				// Neither Comments, nor Pings are open ?>
 				Both comments and pings are currently closed.
 
-				<?php } edit_post_link('Edit this entry','','.'); ?>
-			
+				<?php } edit_post_link('Edit this entry','','.<br />'); ?>
+				<?php previous_post_link('&laquo; %link') ?> <?php next_post_link('%link &raquo;') ?>
 			</footer>
 			
 			<?php comments_template(); ?>
